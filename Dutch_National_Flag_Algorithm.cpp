003c1534c7ae0aad826e8ed369a39683860b0841
HiTShiroya <84978109+HiTShiroya@users.noreply.github.com>
2022-10-08 01:33:56 +0530

    Update Dutch_National_Flag_Algorithm.cpp

--- Dutch_National_Flag_Algorithm.cpp
+++ Dutch_National_Flag_Algorithm.cpp
@@ -1,17 +1,25 @@
-Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+//Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 
-We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+//We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
   
-Input: nums = [2,0,2,1,1,0]
-Output: [0,0,1,1,2,2]
+//Input: nums = [2,0,2,1,1,0]
+//Output: [0,0,1,1,2,2]
 
 
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        int low=0;
+//Code in CPP
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+      cin>>arr[i];
+    }
+      int low=0;
         int mid=0;
-        int n=nums.size();
         int high=n-1;
 
 
@@ -23,10 +31,17 @@ public:
             }else if(nums[mid]==1)mid++;
             else {
                 swap(nums[mid],nums[high]);
-                high--;
+                high--;//if we increment mid pointer then there will be conflicts which can be understood by this 
+                //example  0 0(low) 0 1(mid) 0 1(high) 2
             }
-            // 0 0(low) 0 1(mid) 0 1(high) 2
+            
         }
+        
+        for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    
+}
 
-    }
-};
+    
